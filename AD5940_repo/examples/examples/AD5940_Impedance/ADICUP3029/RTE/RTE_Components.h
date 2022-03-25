9750fde771033dd3fe3e892d97c4c79f55b59dbb
GiovanniN46 <64530317+GiovanniN46@users.noreply.github.com>
2022-03-25 19:06:05 +0100

    Init

---
+++ AD5940_repo/examples/examples/AD5940_Impedance/ADICUP3029/RTE/RTE_Components.h
@@ -0,0 +1,17 @@
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: AD5940_Impedance
+ * RTE configuration: AD5940_Impedance.rteconfig
+*/
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+/*
+ * Define the Device Header File:
+*/
+#define CMSIS_device_header "ADuCM3029.h"
+
+
+#endif /* RTE_COMPONENTS_H */
