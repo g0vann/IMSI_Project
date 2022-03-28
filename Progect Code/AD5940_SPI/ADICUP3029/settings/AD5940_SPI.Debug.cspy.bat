1ae405dcd3ab8b0babf98ae36ac78c3c585ee42a
Papuzzitas-97 <73720682+Papuzzitas-97@users.noreply.github.com>
2022-03-28 23:54:27 +0200

    prove usando iar

--- Progect Code/AD5940_SPI/ADICUP3029/settings/AD5940_SPI.Debug.cspy.bat
+++ Progect Code/AD5940_SPI/ADICUP3029/settings/AD5940_SPI.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files\IAR Systems\Embedded Workbench 9.0\common\bin\cspybat" -f "C:\Users\giann\Desktop\Giovanni\01_Università\Magistrale\SecondoAnno\IMSI\IMSI_Project\Progect Code\AD5940_SPI\ADICUP3029\settings\AD5940_SPI.Debug.general.xcl" --backend -f "C:\Users\giann\Desktop\Giovanni\01_Università\Magistrale\SecondoAnno\IMSI\IMSI_Project\Progect Code\AD5940_SPI\ADICUP3029\settings\AD5940_SPI.Debug.driver.xcl" 
+"C:\Program Files\IAR Systems\Embedded Workbench 9.0\common\bin\cspybat" -f "C:\Users\papuz\OneDrive\Desktop\IMSI_Project\Progect Code\AD5940_SPI\ADICUP3029\settings\AD5940_SPI.Debug.general.xcl" --backend -f "C:\Users\papuz\OneDrive\Desktop\IMSI_Project\Progect Code\AD5940_SPI\ADICUP3029\settings\AD5940_SPI.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files\IAR Systems\Embedded Workbench 9.0\common\bin\cspybat" -f "C:\Users\giann\Desktop\Giovanni\01_Università\Magistrale\SecondoAnno\IMSI\IMSI_Project\Progect Code\AD5940_SPI\ADICUP3029\settings\AD5940_SPI.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\giann\Desktop\Giovanni\01_Università\Magistrale\SecondoAnno\IMSI\IMSI_Project\Progect Code\AD5940_SPI\ADICUP3029\settings\AD5940_SPI.Debug.driver.xcl" 
+"C:\Program Files\IAR Systems\Embedded Workbench 9.0\common\bin\cspybat" -f "C:\Users\papuz\OneDrive\Desktop\IMSI_Project\Progect Code\AD5940_SPI\ADICUP3029\settings\AD5940_SPI.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\papuz\OneDrive\Desktop\IMSI_Project\Progect Code\AD5940_SPI\ADICUP3029\settings\AD5940_SPI.Debug.driver.xcl" 
 
 @echo off 
 :end
