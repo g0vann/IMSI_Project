43babc6f06325851b82916e907c3efb62acd3792
Papuzzitas-97 <73720682+Papuzzitas-97@users.noreply.github.com>
2022-03-31 00:29:25 +0200

    stasera

--- Prova.m
+++ Prova.m
@@ -1,18 +1,32 @@
-close all; clear all;
+close all; clear all; clc;
+% 
+% % serial object
+% s = serial('COM3', 'BaudRate', 230400);
+% % open the serial port
+% fopen(s);
+% % read data
+% %     data = str2double(fscanf(s)); %potremmo utilizzare poi un ciclo while per plottare di continuo
+% %     plot(data);
+% while(1)
+%     fgets(s) 
+% end
+% % close the serial port
+% fclose(s);
+%     
+%     
+%     
+% fwrite(s,10)
+% a=[13 10];
+% caratteri=char(a);
+% c=double(caratteri)
 
-% serial object
-s = serial('COM3', 'BaudRate', 230400);
-% open the serial port
-fopen(s);
-% read data
-%     data = str2double(fscanf(s)); %potremmo utilizzare poi un ciclo while per plottare di continuo
-%     plot(data);
-while(1)
-    fgets(s) 
-end
-% close the serial port
-fclose(s);
-    
-    
-    
-fwrite(s,10)
+% char * temp;
+stringa="ciao";
+len=strlength(stringa);
+len=len+len;
+
+% for i=0 to len
+%     if i==0
+%         temp=stringa();
+%     end
+
