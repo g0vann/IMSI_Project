4c5da55de29db1c7aaae9f2070d4d07da3c59255
Papuzzitas-97 <73720682+Papuzzitas-97@users.noreply.github.com>
2022-04-10 21:25:38 +0200

    tempo

--- tempo.m
+++ tempo.m
@@ -5,15 +5,20 @@ c=clock;
 fix(c);
 data=c;
 
-for i=1:10
-   t = clock;
-   fix(t)
-   data = [data; t];
-   pause(1);
-end
-hours = data(:,4);
-minutes = data(:,5);
-seconds = data(:,6);
-hours
-minutes
-seconds
+hour=data(4);
+minute = data(5);
+second = fix(data(6));
+time = num2str(hour)+":"+num2str(minute)+":"+num2str(second); 
+
+% for i=1:10
+%    t = clock;
+%    fix(t);
+%    data = [data; t];
+%    pause(1);
+% end
+% hours = data(:,4);
+% minutes = data(:,5);
+% seconds = data(:,6);
+% hours
+% minutes
+% seconds
