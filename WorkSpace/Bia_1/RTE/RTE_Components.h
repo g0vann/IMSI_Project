7e56bf89e868c8e4d58593d75122b8da3189392f
Papuzzitas-97 <73720682+Papuzzitas-97@users.noreply.github.com>
2022-05-02 23:39:41 +0200

    ciao

---
+++ WorkSpace/Bia_1/RTE/RTE_Components.h
@@ -0,0 +1,17 @@
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: Bia_1
+ * RTE configuration: Bia_1.rteconfig
+*/
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+/*
+ * Define the Device Header File:
+*/
+#define CMSIS_device_header "ADuCM3029.h"
+
+
+#endif /* RTE_COMPONENTS_H */
