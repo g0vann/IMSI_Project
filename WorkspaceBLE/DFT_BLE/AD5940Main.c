8e98e7c87ad6a7c37cbeca02b0f78a1a47713037
Papuzzitas-97 <73720682+Papuzzitas-97@users.noreply.github.com>
2022-05-19 00:20:37 +0200

    ciao

---
+++ WorkspaceBLE/DFT_BLE/AD5940Main.c
@@ -0,0 +1,235 @@
+/*!
+ *****************************************************************************
+ @file:    AD5940Main.c
+ @author:  Neo Xu
+ @brief:   Used to control specific application and process data.
+ -----------------------------------------------------------------------------
+
+Copyright (c) 2017-2019 Analog Devices, Inc. All Rights Reserved.
+
+This software is proprietary to Analog Devices, Inc. and its licensors.
+By using this software you agree to the terms of the associated
+Analog Devices Software License Agreement.
+ 
+*****************************************************************************/
+#include "ad5940.h"
+#include "AD5940.h"
+
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <math.h>
+#include "ConfigurationBLE.h"
+#include <time.h>
+
+
+void DFT_config(void);
+extern void Function_DFT(void);
+extern void SetAdvertisingMode(void);
+extern void InitBluetoothLowEnergy(void);
+extern int b;
+
+ADI_DATA_PACKET eDataPacket;
+extern bool gbConnected;
+extern ADI_BLE_GAP_MODE geMode;
+
+static const char         sHelloWorld[]   = "Hello World";
+//static const char         sGoodbyeWorld[] = "Bye World";
+
+
+
+#define APPBUFF_SIZE 512
+uint32_t AppBuff[APPBUFF_SIZE];
+
+
+/* Initialize AD5940 basic blocks like clock */
+static int32_t AD5940PlatformCfg(void)
+{
+  CLKCfg_Type clk_cfg;
+  FIFOCfg_Type fifo_cfg;
+  SEQCfg_Type seq_cfg;
+  AGPIOCfg_Type gpio_cfg;
+
+  /* Use hardware reset */
+  AD5940_HWReset();
+  /* Platform configuration */
+  AD5940_Initialize();
+  /* Step1. Configure clock */
+  clk_cfg.ADCClkDiv = ADCCLKDIV_1;
+  clk_cfg.ADCCLkSrc = ADCCLKSRC_HFOSC;
+  clk_cfg.SysClkDiv = SYSCLKDIV_1;
+  clk_cfg.SysClkSrc = SYSCLKSRC_HFOSC;
+  clk_cfg.HfOSC32MHzMode = bFALSE;
+  clk_cfg.HFOSCEn = bTRUE;
+  clk_cfg.HFXTALEn = bFALSE;
+  clk_cfg.LFOSCEn = bTRUE;
+  AD5940_CLKCfg(&clk_cfg);
+  /* Step2. Configure FIFO and Sequencer*/
+  fifo_cfg.FIFOEn = bFALSE;
+  fifo_cfg.FIFOMode = FIFOMODE_FIFO;
+  fifo_cfg.FIFOSize = FIFOSIZE_4KB;                       /* 4kB for FIFO, The reset 2kB for sequencer */
+  fifo_cfg.FIFOSrc = FIFOSRC_DFT;
+  fifo_cfg.FIFOThresh = 4;//AppBIACfg.FifoThresh;        /* DFT result. One pair for RCAL, another for Rz. One DFT result have real part and imaginary part */
+  AD5940_FIFOCfg(&fifo_cfg);                             /* Disable to reset FIFO. */
+	fifo_cfg.FIFOEn = bTRUE;  
+  AD5940_FIFOCfg(&fifo_cfg);                             /* Enable FIFO here */
+  /* Configure sequencer and stop it */
+  seq_cfg.SeqMemSize = SEQMEMSIZE_2KB;
+  seq_cfg.SeqBreakEn = bFALSE;
+  seq_cfg.SeqIgnoreEn = bFALSE;
+  seq_cfg.SeqCntCRCClr = bTRUE;
+  seq_cfg.SeqEnable = bFALSE;
+  seq_cfg.SeqWrTimer = 0;
+  AD5940_SEQCfg(&seq_cfg);
+
+  /* Step3. Interrupt controller */
+  AD5940_INTCCfg(AFEINTC_1, AFEINTSRC_ALLINT, bTRUE);           /* Enable all interrupt in Interrupt Controller 1, so we can check INTC flags */
+  AD5940_INTCCfg(AFEINTC_0, AFEINTSRC_DATAFIFOTHRESH, bTRUE);   /* Interrupt Controller 0 will control GP0 to generate interrupt to MCU */
+  AD5940_INTCClrFlag(AFEINTSRC_ALLINT);
+  /* Step4: Reconfigure GPIO */
+  gpio_cfg.FuncSet = GP6_SYNC|GP5_SYNC|GP4_SYNC|GP2_EXTCLK|GP1_SYNC|GP0_INT;
+  gpio_cfg.InputEnSet = AGPIO_Pin2;
+  gpio_cfg.OutputEnSet = AGPIO_Pin0|AGPIO_Pin1|AGPIO_Pin4|AGPIO_Pin5|AGPIO_Pin6;
+  gpio_cfg.OutVal = 0;
+  gpio_cfg.PullEnSet = 0;
+  AD5940_AGPIOCfg(&gpio_cfg);
+	
+  AD5940_SleepKeyCtrlS(SLPKEY_UNLOCK);  /* Enable AFE to enter sleep mode. */
+
+  /*funzione per la DFT*/
+  
+  //DFT_config();
+  
+  return 0;
+}
+
+void DFT_config(void){
+  DSPCfg_Type dsp_cfg;
+  WGCfg_Type wg_cfg;
+  /* Configure AFE power mode and bandwidth */
+  AD5940_AFEPwrBW(AFEPWR_LP, AFEBW_250KHZ);
+  
+  AD5940_StructInit(&dsp_cfg, sizeof(dsp_cfg));
+  /* Initialize ADC basic function */
+  dsp_cfg.ADCBaseCfg.ADCMuxP = ADCMUXP_VCE0;
+  dsp_cfg.ADCBaseCfg.ADCMuxN = ADCMUXN_VSET1P1;
+  dsp_cfg.ADCBaseCfg.ADCPga = ADCPGA_1;
+  
+  /* Initialize ADC filters ADCRawData-->SINC3-->SINC2+NOTCH */
+  dsp_cfg.ADCFilterCfg.ADCSinc3Osr = ADCSINC3OSR_4;
+  dsp_cfg.ADCFilterCfg.ADCSinc2Osr = ADCSINC2OSR_1333;
+  dsp_cfg.ADCFilterCfg.ADCAvgNum = ADCAVGNUM_2;         /* Don't care about it. Average function is only used for DFT */
+  dsp_cfg.ADCFilterCfg.ADCRate = ADCRATE_800KHZ;        /* If ADC clock is 32MHz, then set it to ADCRATE_1P6MHZ. Default is 16MHz, use ADCRATE_800KHZ. */
+  dsp_cfg.ADCFilterCfg.BpNotch = bTRUE;                 /* SINC2+Notch is one block, when bypass notch filter, we can get fresh data from SINC2 filter. */
+  dsp_cfg.ADCFilterCfg.BpSinc3 = bFALSE;                /* We use SINC3 filter. */
+  dsp_cfg.ADCFilterCfg.Sinc2NotchEnable = bTRUE;        /* Enable the SINC2+Notch block. You can also use function AD5940_AFECtrlS */
+  dsp_cfg.DftCfg.DftNum = DFTNUM_16384;
+  dsp_cfg.DftCfg.DftSrc = DFTSRC_SINC3;
+  AD5940_DSPCfgS(&dsp_cfg);
+
+  AD5940_StructInit(&wg_cfg, sizeof(wg_cfg));
+  wg_cfg.WgType = WGTYPE_SIN;
+  wg_cfg.SinCfg.SinFreqWord = AD5940_WGFreqWordCal(500.0, 16000000.0);  /* 10kHz */
+  AD5940_WGCfgS(&wg_cfg);
+
+  /* Enable all interrupt at Interrupt Controller 1. So we can check the interrupt flag */
+  AD5940_AFECtrlS(AFECTRL_ADCPWR|AFECTRL_SINC2NOTCH|AFECTRL_WG, bTRUE);
+  AD5940_AFECtrlS(AFECTRL_DFT, bTRUE);
+  AD5940_ADCConvtCtrlS(bTRUE);
+}
+
+/*Con la funzione AD5940_Main Accendiamo il bluetooth e la scheda di sopra che da la DFT con la UART*/
+/*
+void AD5940_Main(void)
+{
+   
+  AD5940PlatformCfg(); // Board configuration
+  
+  //OTTIMOOOOOOOO!
+  
+  while(1)
+  {
+      if (b==1){
+          
+        Function_DFT();
+      }
+  }
+}
+*/
+
+/*Se invece commentiamo la funzione di sopra e decommentiamo questa (void AD5940_MainBLE()) proviamo a fare una send ma senza successo*/
+
+
+
+void AD5940_MainBLE(){
+  
+  AD5940PlatformCfg(); // Board configuration
+
+  ADI_BLER_RESULT eResult;  
+  //uint32_t nTime = 0ul;
+  ADI_BLER_CONN_INFO sConnInfo;
+  //float nTempCel, nTempFar;
+  //uint16_t   nTemp;
+  
+  
+  // Bit:0 Sensor Data Packet Bits:1-7 : Sensor ID 
+  eDataPacket.nPacketHeader = ADI_SET_HEADER(ADI_SENSOR_PACKET, DFT_ID);
+
+   //Set Sensor Type 
+  /*eDataPacket.eSensorType = ADI_PRINTSTRING_TYPE;*/
+
+  printf("OTTIMOOOOOOOO!\r\n");
+  
+  while(1)
+  {
+    //printf("ciaoooo!\r\n");
+    eResult = adi_ble_DispatchEvents(ADI_APP_DISPATCH_TIMEOUT);
+    if(eResult==ADI_BLER_FAILURE){
+      printf("Error dispatching events to the callback.\r\n");
+    }
+    
+          if (b==1){
+
+          Function_DFT();
+        }
+
+
+    // If connected, send data 
+    //if(gbConnected == true) {
+     // adi_ble_GetConnectionInfo(&sConnInfo);
+
+         //Get timestamp 
+       /* nTime = (unsigned)time(NULL);
+        memcpy(&eDataPacket.aTimestamp, &nTime, 4u);*/
+        
+        //ASSERT(strlen(sHelloWorld) <= ADI_MAX_STRING_SIZE);
+                        // Set the size of the string 
+        //eDataPacket.eStringData.nStringSize = strlen(sHelloWorld);
+        //Set the string 
+        //memcpy(eDataPacket.eStringData.aStringData, sHelloWorld, strlen(sHelloWorld));
+
+  
+        //eResult = adi_radio_DE_SendData(sConnInfo.nConnHandle, DATAEXCHANGE_PACKET_SIZE, (uint8_t*)&eDataPacket);
+       /* if (eResult != ADI_BLER_SUCCESS) {
+          //printf("Error sending the data.\r\n");
+        }else {
+         // printf("inviato correttamente\r\n");
+        }*/
+        //}
+     // If disconnected switch to advertising mode 
+       /* else {
+            if(geMode != PERIPHERAL_ADV_MODE) {
+                SetAdvertisingMode();
+            }
+        }*/
+    
+  } 
+}
+
+
+
+
+
+
---
+++ WorkspaceBLE/DFT_BLE/main.c
@@ -0,0 +1,441 @@
+/*
+
+Copyright (c) 2017-2019 Analog Devices, Inc. All Rights Reserved.
+
+This software is proprietary to Analog Devices, Inc. and its licensors.
+By using this software you agree to the terms of the associated
+Analog Devices Software License Agreement.
+
+*/
+
+#include "stdio.h"
+#include "ADuCM3029.h"
+#include "AD5940.h"
+#include "adi_initialize.h"
+#include "ConfigurationBLE.h"
+#include <radio/adi_ble_radio.h>
+#include "adi_ble_radio_internal.h"
+
+
+extern ADI_BLE_RADIO_Device *pBLERadio;
+
+/* Functions that used to initialize MCU platform */
+uint32_t MCUPlatformInit(void *pCfg);
+extern int32_t adi_initComponents(void);
+void Received_Data(void);
+
+/**********************************************************
+
+
+
+----------------------BLE---------------------------------
+
+
+
+
+**********************************************************/
+/* Peripheral advertisement mode */
+#define PERIPHERAL_ADV_MODE      ((ADI_BLE_GAP_MODE)(ADI_BLE_GAP_MODE_CONNECTABLE | ADI_BLE_GAP_MODE_DISCOVERABLE))
+
+/* Global Data */
+bool gbConnected;
+ADI_BLE_GAP_MODE   geMode;
+
+
+/* Local Functions used when Bluetooth is enabled */
+static void ApplicationCallback(void * pCBParam, uint32_t Event, void * pArg);
+void InitBluetoothLowEnergy(void);
+void SetAdvertisingMode(void);
+
+
+
+/* Local Functions */
+static void Trap(void);
+/**********************************************************
+**********************************************************/
+
+int main(void)
+{
+ // void AD5940_Main(void); //con questa abbiamo accensione bluetooth e DFT sull'uart
+  void AD5940_MainBLE(void); // con questa proviamo a fare una send con il bluetooth
+  MCUPlatformInit(0);
+  adi_initComponents();
+  AD5940_MCUResourceInit(0);
+  
+  InitBluetoothLowEnergy();
+  
+  //printf("Hello AD5940-Build Time:%s\n",__TIME__);
+  //AD5940_Main();// questa viene richiamata quando usiamo anche la DFt
+  AD5940_MainBLE(); //Questa quando proviamo la send
+}
+
+/* Below functions are used to initialize MCU Platform */
+uint32_t MCUPlatformInit(void *pCfg)
+{
+  int UrtCfg(int iBaud);
+
+  /*Stop watch dog timer(ADuCM3029)*/
+  pADI_WDT0->CTL = 0xC9;
+  /* Clock Configure */
+  pADI_CLKG0_OSC->KEY = 0xCB14;               // Select HFOSC as system clock.
+  pADI_CLKG0_OSC->CTL =                       // Int 32khz LFOSC selected in LFMUX
+    BITM_CLKG_OSC_CTL_HFOSCEN|BITM_CLKG_OSC_CTL_HFXTALEN;
+
+  while((pADI_CLKG0_OSC->CTL&BITM_CLKG_OSC_CTL_HFXTALOK) == 0);
+
+  pADI_CLKG0_OSC->KEY = 0xCB14; 
+  pADI_CLKG0_CLK->CTL0 = 0x201;                   /* Select XTAL as system clock */
+  pADI_CLKG0_CLK->CTL1 = 0;                   // ACLK,PCLK,HCLK divided by 1
+  pADI_CLKG0_CLK->CTL5 = 0x00;                 // Enable clock to all peripherals - no clock gating
+
+  UrtCfg(230400);/*Baud rate: 230400*/
+  return 1;
+}
+
+/**
+	@brief int UrtCfg(int iBaud, int iBits, int iFormat)
+			==========Configure the UART.
+	@param iBaud :{B1200,B2200,B2400,B4800,B9600,B19200,B38400,B57600,B115200,B230400,B430800}	\n
+		Set iBaud to the baudrate required:
+		Values usually: 1200, 2200 (for HART), 2400, 4800, 9600,
+		        19200, 38400, 57600, 115200, 230400, 430800, or type in baud-rate directly
+	@note
+		- Powers up UART if not powered up.
+		- Standard baudrates are accurate to better than 0.1% plus clock error.\n
+		- Non standard baudrates are accurate to better than 1% plus clock error.
+   @warning - If an external clock is used for the system the ullRtClk must be modified with \n
+         the speed of the clock used.
+**/
+
+int UrtCfg(int iBaud)
+{
+  int iBits = 3;//8bits, 
+  int iFormat = 0;//, int iBits, int iFormat
+  int i1;
+  int iDiv;
+  int iRtC;
+  int iOSR;
+  int iPllMulValue;
+  unsigned long long ullRtClk = 16000000;                // The root clock speed
+
+
+  /*Setup P0[11:10] as UART pins*/
+  pADI_GPIO0->CFG = (1<<22)|(1<<20)|(pADI_GPIO0->CFG&(~((3<<22)|(3<<20))));
+
+  iDiv = (pADI_CLKG0_CLK->CTL1& BITM_CLKG_CLK_CTL1_PCLKDIVCNT);                 // Read UART clock as set by CLKCON1[10:8]
+  iDiv = iDiv>>8;
+  if (iDiv == 0)
+    iDiv = 1;
+  iRtC = (pADI_CLKG0_CLK->CTL0& BITM_CLKG_CLK_CTL0_CLKMUX); // Check what is the root clock
+
+  switch (iRtC)
+  {
+  case 0:                                               // HFOSC selected
+    ullRtClk = 26000000;
+    break;
+
+  case 1:                                               // HFXTAL selected
+    if ((pADI_CLKG0_CLK->CTL0 & 0x200)==0x200)           // 26Mhz XTAL used
+        ullRtClk = 26000000;
+    else
+        ullRtClk = 16000000;                              // Assume 16MHz XTAL
+    break;
+
+  case 2:                                               // SPLL output
+    iPllMulValue = (pADI_CLKG0_CLK->CTL3 &             // Check muliplication factor in PLL settings
+                    BITM_CLKG_CLK_CTL3_SPLLNSEL);      // bits[4:0]. Assume div value of 0xD in bits [14:11]
+    ullRtClk = (iPllMulValue *1000000);                // Assume straight multiplication by pADI_CLKG0_CLK->CTL3[4:0]
+    break;
+
+  case 3:
+    ullRtClk = 26000000;                                //External clock is assumed to be 26MhZ, if different
+    break;                                             //clock speed is used, this should be changed
+
+  default:
+    break;
+  }
+  //   iOSR = (pADI_UART0->COMLCR2 & 0x3);
+  //   iOSR = 2^(2+iOSR);
+  pADI_UART0->COMLCR2 = 0x3;
+  iOSR = 32;
+  //i1 = (ullRtClk/(iOSR*iDiv))/iBaud;	              // UART baud rate clock source is PCLK divided by OSR
+  i1 = (ullRtClk/(iOSR*iDiv))/iBaud-1;   //for bigger M and N value
+  pADI_UART0->COMDIV = i1;
+
+  pADI_UART0->COMFBR = 0x8800|(((((2048/(iOSR*iDiv))*ullRtClk)/i1)/iBaud)-2048);
+  pADI_UART0->COMIEN = 0;
+  pADI_UART0->COMLCR = (iFormat&0x3c)|(iBits&3);
+
+
+  pADI_UART0->COMFCR = (BITM_UART_COMFCR_RFTRIG & 0/*RX_FIFO_1BYTE*/ ) |BITM_UART_COMFCR_FIFOEN;
+  pADI_UART0->COMFCR |= BITM_UART_COMFCR_RFCLR|BITM_UART_COMFCR_TFCLR;                                   // Clear the UART FIFOs
+  pADI_UART0->COMFCR &= ~(BITM_UART_COMFCR_RFCLR|BITM_UART_COMFCR_TFCLR);                                // Disable clearing mechanism
+
+  NVIC_EnableIRQ(UART_EVT_IRQn);              // Enable UART interrupt source in NVIC
+  pADI_UART0->COMIEN = BITM_UART_COMIEN_ERBFI|BITM_UART_COMIEN_ELSI; /* Rx Interrupt */
+  return pADI_UART0->COMLSR;
+}
+#include "stdio.h"
+#ifdef __ICCARM__
+int putchar(int c)
+#else
+int fputc(int c, FILE *f)
+#endif
+{
+  pADI_UART0->COMTX = c;
+  while((pADI_UART0->COMLSR&0x20) == 0);// tx fifo empty
+  return c;
+}
+
+void UART_Int_Handler(void)
+{
+  void UARTCmd_Process(char);
+  uint32_t flag;
+  flag = pADI_UART0->LSR; //Line Status Register 16bit register (it indicates if there have been transmission errors)
+  flag = pADI_UART0->IIR; //Interrupt ID register 16bit register
+  
+  // Now flag has 32 bit and give information about rx
+  
+  // First case: all bytes are recieved
+  if((flag & 0x0e) == 0x04)  /* Receive Byte */
+  {
+    uint32_t count;
+    count = pADI_UART0->RFC;  /* RFC Register Receive FIFO data bytes (received characters) count */
+    for(int i=0;i < count; i++)
+    {
+      char c;
+      c = pADI_UART0->COMRX&0xff; //COMRX contains the received character
+      UARTCmd_Process(c); // this function recognizes received data
+    }
+  }
+  
+  // Second case: trasmission timeout, processing only what we have had
+  if((flag & 0x0e) == 0xc)  /* Time-out */
+  {
+    uint32_t count;
+    count = pADI_UART0->RFC;  /* Receive FIFO count */
+    for(int i=0;i < count; i++)
+    {
+      char c;
+      c = pADI_UART0->COMRX&0xff;
+      UARTCmd_Process(c);
+    }
+  }
+}
+
+
+/**********************************************************************
+***********************************************************************/
+
+/*!
+ * @brief      Trap function
+ *
+ * @details    In case of catastrophic errors this function is called to block
+ *             infinitely.
+ */
+static void Trap()
+{
+    while(1);
+}
+
+/*!
+ * @brief      Set Advertising Mode
+ *
+ * @details    Helper function to avoid repeated code in main.
+ *
+ */
+void SetAdvertisingMode(void)
+{
+    ADI_BLER_RESULT eResult;
+
+    eResult = adi_radio_SetMode(PERIPHERAL_ADV_MODE, 0u, 0u);
+    if(eResult==ADI_BLER_SUCCESS){
+      printf("Mode set successfully\r\n");
+    }else{
+      printf("Error setting the mode.\r\n");
+    }
+    
+    eResult = adi_ble_WaitForEventWithTimeout(GAP_EVENT_MODE_CHANGE, 5000u);
+    if(eResult==ADI_BLER_SUCCESS){
+      printf("Waiting an event\r\n");
+    }else{
+      printf("Error waiting for GAP_EVENT_MODE_CHANGE.\r\n");
+    }   
+
+    eResult = adi_radio_GetMode(&geMode);
+    if(eResult==ADI_BLER_SUCCESS){
+      printf("Mode obtained\r\n");
+    }else{
+      printf("Error getting the mode.\r\n");
+    }       
+
+    if(geMode != PERIPHERAL_ADV_MODE) {
+       printf("Error in SetAdvertisingMode.\r\n");
+    }
+}
+
+/*!
+ * @brief      Initializes the bluetooth
+ *
+ * @details    Data Exchange profile is initialized to send
+ *             data to the connected central device.
+ */
+void InitBluetoothLowEnergy(void)
+{
+    ADI_BLER_RESULT eResult;
+    uint8_t *       aDataExchangeName = (unsigned char *) "FrancOlino_BLE";
+
+    /* Initialize radio and framework layer */
+    eResult = adi_ble_Init(ApplicationCallback, NULL);
+    if(eResult==ADI_BLER_SUCCESS){
+      printf("Initialization successful\r\n");
+    }else{
+      printf("Error initializing the radio.\r\n");
+    }
+    /* Configure radio */
+    eResult = adi_radio_RegisterDevice(ADI_BLE_ROLE_PERIPHERAL);
+    if(eResult==ADI_BLER_SUCCESS){
+      printf("Registered radio\r\n");
+    }else{
+      printf("Error registering the radio.\r\n");
+    }
+
+    eResult = adi_radio_SetLocalBluetoothDevName(aDataExchangeName, strlen((const char *) aDataExchangeName), 0u, 0u);
+    if(eResult==ADI_BLER_SUCCESS){
+      printf("Local device name set successfull\r\n");
+    }else{
+      printf("Error setting local device name.\r\n");
+    }
+
+    SetAdvertisingMode();
+
+    /* Initialize data exchange profile */
+    eResult = adi_radio_Register_DataExchangeServer();
+    if(eResult==ADI_BLER_SUCCESS){
+      printf("Data exchange server registrated\r\n");
+    }else{
+      printf("Error registering data exchange server.\r\n");
+    }    
+
+    /* Now enter infinite loop waiting for connection and then data exchange events */
+    printf("Waiting for connection. Initiate connection on central device please.\r\n");
+}
+
+
+static void ApplicationCallback(void * pCBParam, uint32_t Event, void * pArg)
+{
+    switch(Event) {
+        case GAP_EVENT_CONNECTED:
+            printf("Connected!\r\n");
+            gbConnected = true;
+            break;
+
+        case GAP_EVENT_DISCONNECTED:
+            printf("Disconnected!\r\n");
+            geMode      = ADI_BLE_GAP_MODE_NOTCONNECTABLE;
+            gbConnected = false;
+            break;
+
+        case DATA_EXCHANGE_RX_EVENT:
+            printf("Data received!\r\n");
+            Received_Data();
+            break;
+
+        case DATA_EXCHANGE_TX_COMPLETE:
+            printf("Data sent!\r\n");
+            break;
+
+        case GAP_EVENT_MODE_CHANGE:
+            printf("GAP mode changed.\r\n");
+            break;
+
+        case GAP_EVENT_CONNECTION_UPDATED:
+            printf("Connection interval updated.\r\n");
+            break;
+
+        case BLE_RADIO_ERROR_READING:
+            /* If you want to enable this print statement, please be aware that the first
+             * packet sent from the radio on startup will cause this error. It is a known bug
+             * and will not have any adverse effects on the application.
+             *
+             *DEBUG_MESSAGE("Failed to read a packet from the radio.\r\n");
+             *
+             */
+            break;
+
+        case BLE_RADIO_ERROR_PARSING:
+            printf("Failed to parse a packet from the radio.\r\n");
+            break;
+
+        case BLE_RADIO_ERROR_PROCESSING:
+            printf("Failed to process a packet from the radio.\r\n");
+            break;
+
+        default:
+           // printf("Unexpected event received.\r\n");
+            break;
+    }
+}
+
+void Received_Data(void){
+  void BLECmd_Process(char []);
+  char aStringReceive[20];
+  char aTemp[20];
+  
+  for(int i = 0;i<20;i++){
+    aTemp[i] = pBLERadio->rxDataPkt.rxPkt[i];
+  }
+  
+  for (int i = 0;i<20;i++){
+    aStringReceive[i] = aTemp [19-i];  
+  }
+  
+  for(int i = 0;i<20;i++){  
+  printf("%c ",aStringReceive[i]);
+  if(i==19)
+    printf("\r\n");
+  }
+  BLECmd_Process(aStringReceive);
+
+  
+}
+
+/*
+void UART_Int_Handler(void)
+{
+  void UARTCmd_Process(char);
+  uint32_t flag;
+  flag = pADI_UART0->LSR; //Line Status Register 16bit register (it indicates if there have been transmission errors)
+  flag = pADI_UART0->IIR; //Interrupt ID register 16bit register
+  
+  // Now flag has 32 bit and give information about rx
+  
+  // First case: all bytes are recieved
+  if((flag & 0x0e) == 0x04)  //Receive Byte 
+  {
+    uint32_t count;
+    count = pADI_UART0->RFC;  // RFC Register Receive FIFO data bytes (received characters) count 
+    for(int i=0;i < count; i++)
+    {
+      char c;
+      c = pADI_UART0->COMRX&0xff; //COMRX contains the received character
+      UARTCmd_Process(c); // this function recognizes received data
+    }
+  }
+  
+  // Second case: trasmission timeout, processing only what we have had
+  if((flag & 0x0e) == 0xc)  //Time-out 
+  {
+    uint32_t count;
+    count = pADI_UART0->RFC;  // Receive FIFO count 
+    for(int i=0;i < count; i++)
+    {
+      char c;
+      c = pADI_UART0->COMRX&0xff;
+      UARTCmd_Process(c);
+    }
+  }
+}
+*/
+
+
