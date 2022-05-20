78272ba9c2c2d4aa194be1beb1c93c6c8ba4fa45
Papuzzitas-97 <73720682+Papuzzitas-97@users.noreply.github.com>
2022-05-20 19:04:48 +0200

    ciaocoaicoaioiocioa

--- Prova2.m
+++ Prova2.m
@@ -2,7 +2,11 @@ clear
 clc
 
 
-ciao1 = char("154.000");
-ciao2 = char("4561.2004");
-data = ciao1+ " " + ciao2;
-data1 = char(data);
+% ciao1 = char("154.000");
+% ciao2 = char("4561.2004");
+% data = ciao1+ " " + ciao2;
+% data1 = char(data);
+
+ciao1 = [5 6 7 898 54 654 0 0 0 56 54];
+ciao2 = uint8(ciao1(2:5));
+Afloat = typecast( ciao2, 'single');
--- ciao.m
+++ ciao.m
@@ -1,11 +1,16 @@
-function [data,timestamp] = ciao(src,evt)
+function [ciao1,timestamp] = ciao(src,evt)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 [ciao1, timestamp] = read(src, "oldest");
-data = char(ciao1);
-elemento = split(data);
-data =(elemento(1))+ " " + (elemento(2));% stacco cmq non va bene dobbiamo mandare la lunghezza o settare da firmware che devono essere mandati spazi fino alla chiusura della stringa
-data = char (data);
-disp(data)
+% data = char(ciao1);
+% elemento = split(data);
+% data =(elemento(1))+ " " + (elemento(2));% stacco cmq non va bene dobbiamo mandare la lunghezza o settare da firmware che devono essere mandati spazi fino alla chiusura della stringa
+% data = char (data);
+ciao2 = uint8(ciao1(2:5));
+ciao3 = uint8(ciao1(6:9));
+Afloat = typecast( ciao2, 'single');
+Bfloat = typecast( ciao3, 'single');
+disp([Afloat Bfloat])
+
 end
 
